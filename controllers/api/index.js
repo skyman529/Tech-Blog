c8852a605d5731aab8ab75a39fff9e738570efea
skyman529 <skyman529@gmail.com>
2023-02-03 15:52:52 -0700

    finished

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -3,8 +3,8 @@ const userRoutes = require("./userRoutes");
 const blogRoutes = require("./blogRoutes");
 const commentRoutes = require("./commentRoutes");
 
-router.use("/users", userRoutes);
-router.use("./blogposts", blogRoutes);
-router.use("./comments", commentRoutes);
+router.use("/user", userRoutes);
+router.use("/blogposts", blogRoutes);
+router.use("/comments", commentRoutes);
 
 module.exports = router;
